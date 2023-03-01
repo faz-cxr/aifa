360d9944c4dccd30cc5de83847acaef48a1d4071
faz-cxr <47987464+faz-cxr@users.noreply.github.com>
2023-03-01 15:19:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,9 +68,9 @@ if "past" not in st.session_state:
 
 def get_text():
     st.markdown("""
-    # Curious about your health? Aifa has answers!
+    # Curious about your health? \nAifa has answers!
     """)
-    input_text = st.text_input("Ask anything medical-related!\nRefresh the page to reset the conversation...", disabled=False, placeholder="Start typing here and press enter ⏎", key="input")
+    input_text = st.text_input("Refresh the page to reset the conversation...", disabled=False, placeholder="Start typing here and press enter ⏎", key="input")
     return input_text
 
 hide="""
